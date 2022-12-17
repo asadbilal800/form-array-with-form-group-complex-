d13d8ebcc43e4a6704ef5a8c3a5e2fdebe71bf84
asadbilal800 <asadbilal800@gmail.com>
2022-12-17 21:00:21 +0500

    added another form array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,47 +16,44 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-   let element = this.form.get('websites').valueChanges.subscribe(response => {
-     console.log(this.form)
-   })
-
   }
 
   get formArray() {
     return this.form.get('websites') as FormArray;
   }
 
+   formLinkArray(index){
+     console.log(this.form)
+     console.log(this.form.get('websites').at(index).get('LinkArray') as FormArray)
+    return this.form.get('websites').at(index).get('LinkArray') as FormArray
+  }
+
   generateFormArrayElement() {
     return new FormGroup({
-      Link: new FormControl('')
-    })
+      Link: new FormControl(''),
+      LinkArray: new FormArray([new FormControl('')])
+    });
   }
 
-  changeWebsite(value: string,index:number){
-
-    if(this.checkIfLastElement(index) && value){
-      this.formArray.push(this.generateFormArrayElement())
-      let secondlastElementId = 'website'+ (this.formArray.length -2)
+  changeWebsite(value: string, index: number) {
+    if (this.checkIfLastElement(index) && value) {
+      this.formArray.push(this.generateFormArrayElement());
+      let secondlastElementId = 'website' + (this.formArray.length - 2);
       setTimeout(() => {
         document.getElementById(secondlastElementId).focus();
-      },10)
+      }, 10);
+    } else if (this.checkIfSecondLastElement(index) && !value) {
+      this.formArray.removeAt(index + 1);
+    } else if (!value) {
+      this.formArray.removeAt(index);
     }
-    else if(this.checkIfSecondLastElement(index) && !value) {
-    this.formArray.removeAt(index + 1)
-    }
-
-    else if(!value) {
-      this.formArray.removeAt(index)
-    }
-
-
   }
-  
+
   checkIfSecondLastElement(index: number) {
-    return (this.formArray.length -2 === index) ? true : false 
+    return this.formArray.length - 2 === index ? true : false;
   }
 
-  checkIfLastElement(index:number){
-    return (this.formArray.length -1 === index) ? true : false 
+  checkIfLastElement(index: number) {
+    return this.formArray.length - 1 === index ? true : false;
   }
 }
