9dbe4bffed801628afab4baa1842f633879ffe8f
asadbilal800 <asadbilal800@gmail.com>
2022-12-17 20:14:08 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# form-array-with-form-group-complex-
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-forms-formarray-example-fmccdh)
---
+++ src/app/app.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  form: FormGroup;
+  constructor() {
+    this.form = new FormGroup({
+      username: new FormControl('asad'),
+      websites: new FormArray([this.generateFormArrayElement()]),
+    });
+  }
+
+  ngOnInit(): void {
+   let element = this.form.get('websites').valueChanges.subscribe(response => {
+     console.log(this.form)
+   })
+
+  }
+
+  get formArray() {
+    return this.form.get('websites') as FormArray;
+  }
+
+  generateFormArrayElement() {
+    return new FormGroup({
+      Link: new FormControl('')
+    })
+  }
+
+  changeWebsite(value: string,index:number){
+
+    if(this.checkIfLastElement(index) && value){
+      this.formArray.push(this.generateFormArrayElement())
+      let secondlastElementId = 'website'+ (this.formArray.length -2)
+      setTimeout(() => {
+        document.getElementById(secondlastElementId).focus();
+      },10)
+    }
+    else if(this.checkIfSecondLastElement(index) && !value) {
+    this.formArray.removeAt(index + 1)
+    }
+
+    else if(!value) {
+      this.formArray.removeAt(index)
+    }
+
+
+  }
+  
+  checkIfSecondLastElement(index: number) {
+    return (this.formArray.length -2 === index) ? true : false 
+  }
+
+  checkIfLastElement(index:number){
+    return (this.formArray.length -1 === index) ? true : false 
+  }
+}
